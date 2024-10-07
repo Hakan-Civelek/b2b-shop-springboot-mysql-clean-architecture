0898bcab2a1666f3f7af1c20045d49f3a5387b78
Hakan CİVELEK <hakancivelek5461@gmail.com>
2024-10-07 23:57:26 +0300

    initial commit

---
+++ src/main/java/com/hakancivelek/b2bshop/cleanarch/CleanarchApplication.java
@@ -0,0 +1,13 @@
+package com.hakancivelek.b2bshop.cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchApplication.class, args);
+	}
+
+}
